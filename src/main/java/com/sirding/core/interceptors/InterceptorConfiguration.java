9bac84d461a047f183800c3fa8689af3f1a6193c
dingzc <zhichaoding@hongkun.com.cn>
2017-01-13 15:22:12 +0800

    将拦截器配置从注解方式更新为xml配置

--- src/main/java/com/sirding/core/interceptors/InterceptorConfiguration.java
+++ src/main/java/com/sirding/core/interceptors/InterceptorConfiguration.java
@@ -1,12 +1,16 @@
 package com.sirding.core.interceptors;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
-@EnableWebMvc
+/**
+ * @Described	: <span>@EnableWebMvc</span>会导致单元测试的出现异常，因此将拦截器配置移入到spring-mvc.xml文件中进行配置
+ * @project		: com.sirding.core.interceptors.InterceptorConfiguration
+ * @author 		: zc.ding
+ * @date 		: 2017年1月13日
+ */
+//@Configuration
+//@EnableWebMvc
 public class InterceptorConfiguration extends WebMvcConfigurerAdapter {
 
 	@Override
--- src/test/java/com/sirding/testutil/PwdUtilTest.java
+++ src/test/java/com/sirding/testutil/PwdUtilTest.java
@@ -1,8 +1,9 @@
 package com.sirding.testutil;
 
 import java.security.MessageDigest;
+import java.util.UUID;
 
-import org.apache.shiro.codec.Hex;
+import org.apache.commons.codec.binary.Hex;
 import org.junit.Test;
 
 import com.sirding.core.utils.secure.PwdUtil;
@@ -10,39 +11,50 @@ import com.sirding.core.utils.secure.PwdUtil;
 public class PwdUtilTest {
 
 	private static final int DEFAULT_ITERATIONS = 1;
-	
-	
+
+
 	@Test
 	public void test(){
 		byte[] bytes = "123".getBytes();
 		byte[] salt = "i am sirding".getBytes();
 		try {
-//			MessageDigest digest = MessageDigest.getInstance("SHA-1");
+			//			MessageDigest digest = MessageDigest.getInstance("SHA-1");
 			MessageDigest digest = MessageDigest.getInstance("MD5");
 			if (salt != null) {
-	            digest.reset();
-//	            digest.update(salt);
-	        }
-	        byte[] hashed = digest.digest(bytes);
-	        int iterations = 1 - DEFAULT_ITERATIONS; //already hashed once above
-	        //iterate remaining number:
-	        for (int i = 0; i < iterations; i++) {
-	            digest.reset();
-	            hashed = digest.digest(hashed);
-	        }
-	        String msg = Hex.encodeToString(hashed);
-	        System.out.println(msg);
-	        System.out.println(msg.length());
+				digest.reset();
+				//	            digest.update(salt);
+			}
+			byte[] hashed = digest.digest(bytes);
+			int iterations = 1 - DEFAULT_ITERATIONS; //already hashed once above
+			//iterate remaining number:
+			for (int i = 0; i < iterations; i++) {
+				digest.reset();
+				hashed = digest.digest(hashed);
+			}
+			String msg = Hex.encodeHexString(hashed);
+			System.out.println(msg);
+			System.out.println(msg.length());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	@Test
 	public void test2(){
 		String msg = "sirding";
 		String pwd = PwdUtil.encrypt(msg).toString();
 		System.out.println(pwd);
 	}
+
+	@Test
+	public void test3()	{
+		String uuid = UUID.randomUUID().toString();
+		String msg = Hex.encodeHexString(uuid.getBytes());
+		System.out.println(uuid);
+		System.out.println(uuid.length());
+		System.out.println(msg);
+		System.out.println(msg.length());
+	}
+
 }
